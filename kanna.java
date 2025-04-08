ffcd6f2286db1a7cf4ad91fbea5561c44a498f2e
Kannaperumal k <kannaperumal879@gmail.com>
2025-04-08 15:40:58 +0530

    array
    simple array program  array

---
+++ kanna.java
@@ -0,0 +1,12 @@
+public class SimpleArrayExample {
+    public static void main(String[] args) {
+        // Declare and initialize an array of integers
+        int[] numbers = {10, 20, 30, 40, 50};
+
+        // Print the elements of the array
+        System.out.println("Elements of the array:");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println("Element at index " + i + ": " + numbers[i]);
+        }
+    }
+}
