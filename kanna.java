4985d4d2992fc00a0e3748fff7e24f0aa0295f5e
Kannaperumal k <kannaperumal879@gmail.com>
2025-04-21 15:10:29 +0530

    Add files via upload

--- kanna.java
+++ kanna.java
@@ -1,12 +1,17 @@
-public class SimpleArrayExample {
-    public static void main(String[] args) {
-        // Declare and initialize an array of integers
-        int[] numbers = {10, 20, 30, 40, 50};
-
-        // Print the elements of the array
-        System.out.println("Elements of the array:");
-        for (int i = 0; i < numbers.length; i++) {
-            System.out.println("Element at index " + i + ": " + numbers[i]);
+import java.util.Scanner;
+class kanna
+{
+    public static void main(String args[])
+    {
+        for(int i=5;i<=5;i--)
+        {
+            for(int j=1;j<=i;j++)
+            {
+                System.out.print("*");
+            }
+             System.out.println();
         }
+    
+        
     }
 }
